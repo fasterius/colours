ebb9345b783fef442b30c7a0ee09123cea8e5cd8
Erik Fasterius <erik.fasterius@outlook.com>
2019-01-04 12:15:23 +0100

    Add show_colours.sh

---
+++ show_colours.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#   This file echoes a bunch of color codes to the  terminal to demonstrate 
+#   what's available.  Each line is the color code of one forground color,
+#   out of 17 (default + 16 escapes), followed by a test use of that color on 
+#   all nine background colors (default + 8 escapes).
+#
+#   The original version of the first part of this script is credited to 
+#   Daniel Crisman.
+#
+
+T='gYw'   # The test text
+
+echo -e "\n                 40m     41m     42m     43m\
+     44m     45m     46m     47m";
+
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
+           '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
+           '  36m' '1;36m' '  37m' '1;37m';do 
+    
+    FG=${FGs// /}
+  	echo -en " $FGs \033[$FG  $T  "
+  	
+    for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+    	do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m";
+  	done
+	
+    echo ""
+
+done
+
+echo ""
+
+
+# Show colours for each c16 colour rather than ANSI codes
+for FG in {1..17}; do
+    
+    NUM=`expr $FG - 1`
+    echo -en "$NUM Text $(tput setaf $FG)"
+
+    echo ""
+
+done
+
+echo ""
